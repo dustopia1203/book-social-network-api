2782240fc795b4a543787dba421aa16391b9bc72
HieuNguyen <dustopia@devfuseengr.com>
2024-08-18 15:08:50 +0700

    refactor(prj): some small changes (2)

--- src/main/java/com/dustopia/book_social_network_api/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/dustopia/book_social_network_api/security/jwt/JwtAuthenticationFilter.java
@@ -8,17 +8,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
